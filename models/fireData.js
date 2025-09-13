28e0c536c79e1b5a649bf364ec403057f6bc9727
Pranjal Maithani <pranjalmaithani81822@gmail.com>
2025-09-13 15:58:53 +0530

    ff

--- models/fireData.js
+++ models/fireData.js
@@ -1,11 +1,11 @@
 const mongoose = require ('mongoose');
 
 const alertSchema =  new mongoose.Schema({
-    isfire:{type:Boolean, required:true},
     latitude:{type:Number,required:true},
     longitude:{type:Number,required:true},
     humidity:{type:Number,required:true},
     temp:{type:Number,required:true},
+    isfire:{type:Boolean, required:true},
     createdAt:{type:Date, default:Date.now}
 });
 
